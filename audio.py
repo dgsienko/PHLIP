a761a5c274d1fe7467bf40282265a0617fd0bea5
dgsienko <dgsienko@bu.edu>
2016-12-06 13:21:44 -0500

    added comments and cleaned up code

--- audio.py
+++ audio.py
@@ -1,4 +1,4 @@
-#Audio Import and Analysis
+#Audio Import, Analysis, and Main for Audio Visualization
 
 import lights
 import requests
@@ -12,120 +12,122 @@ import multiprocessing
 import random
 
 def get_song_list(cid,artist_name):
-    #gets list of songs from input artist
+    #Gets list of songs from input artist
     
-    #following three lines make api call and get back dictionary of all songs (and data from songs) by the artist
+    #Make api call and get dictionary of all songs (and data from songs) by the artist
     url = "https://api.jamendo.com/v3.0/tracks/?client_id="+str(cid)+"&artist_name="+artist_name
     response = requests.get(url)
     artist_json = response.json()
     
-    #makes a list of all songs by the artist
+    #Makes a list of all songs by the artist
     song_list = [artist_json['results'][i]['name'] for i in range(len(artist_json['results']))]
-    
     return(song_list,artist_json)    
     
 def get_songid(song_name):
-    #gets song id from Jamendo database
+    #Gets song id from Jamendo database
     
-    #gets list of songs by artist and the artist data
+    #Gets list of songs by artist and the artist data
     song_list,artist_json = get_song_list(cid,artist_name) 
     
-    #finds index of song title in list
+    #Finds index of song title in list
     for i in range(len(song_list)):     
         if song_list[i] == song_name:
             y = i
             break
     
-    #finds jamendo song id in artist json
+    #Finds and returns jamendo song id in artist json
     song_id = artist_json['results'][y]['id'] 
-    
     return(song_id)
 
 def get_song():
     #Downloads mp3 of song
     
-    #gets song_id
+    #Gets song_id
     song_id = get_songid(song_name)  
     url = "https://api.jamendo.com/v3.0/tracks/file/?client_id="+str(cid)+"&id="+str(song_id)+"&action=download"
-    #names downloaded file, as of now downloads to desktop
+    #Names downloaded file
     fname = str(artist_name)+" - "+str(song_name)+".mp3"  
     
-    #downloads song 
+    #Downloads song 
     content = requests.get(url, stream=True).content   
     with open(fname, 'wb') as f:
         f.write(content)
         
 def mp3_to_wav(in_fname):
-    #converts mp3 to wav file
-    
-    #convert to wav
+    #Converts mp3 to wav file, saves new file in same place
     sound = AudioSegment.from_mp3(in_fname)
     sound.export(in_fname[:-3] + "wav", format="wav")
     
 def wav_analyzer_fft(fname,threshold):
+    #Analyzes wav file
     
+    #Reads in wav file, singles out on mono files from stereo input
     rate, data = scipy.io.wavfile.read(fname)
-    signal = data[:,0]
-     
+    signal = data[:,0]   
+	#Creates window for considering small portions
     w = rate//10
     len_sig = int(len(signal)//w)
-        
-    blah = []
-    blah_prev = []
-    blah_ffts = []
+    
+	#Loops through signal, takes fft of each window, adds to list of ffts
+    wind = []
+    wind_prev = []
+    wind_ffts = []
     for i in range(len_sig):
         for j in range((i*w),((i*w)+w)):
-            blah += [signal[j]]
-            blah_prev += [signal[j]]
+            wind += [signal[j]]
+            wind_prev += [signal[j]]
         if i == len_sig - 1:
-            if len(blah_prev) == len(blah):
-                blah_ffts += [np.fft.fft(blah)]
-                blah = []
+            if len(wind_prev) == len(wind):
+                wind_ffts += [np.fft.fft(wind)]
+                wind = []
         else:
-            blah_ffts += [np.fft.fft(blah)]
-            blah = []
+            wind_ffts += [np.fft.fft(wind)]
+            wind = []
     
+	#Creates list(returns list in seconds) of places in song where there is some significant frequency difference
     plan = []
-    for i in range(1,len(blah_ffts)):
-        if (sum(blah_ffts[i] - blah_ffts[i-1])/sum(blah_ffts[i-1])) * 100 > threshold:
+    for i in range(1,len(wind_ffts)):
+        if (sum(wind_ffts[i] - wind_ffts[i-1])/sum(wind_ffts[i-1])) * 100 > threshold:
             plan += [i*w]
-    
     plan_sec = [plan[i]/44100 for i in range(0,len(plan))]
     return plan_sec
 
 def play_song(fname,song_ready,color_ready):
-    #plays song
+    #Plays song
 
+	#Readies song and color for multiprocessing
     song_ready.set() 
     color_ready.wait()
     
-    #time,wait(some time) <= if necessary to sync
-    
+    #time,wait(some time) <= if necessary to sync 
     chunk = 1024
     wf = wave.open(fname, 'rb')
     p = pyaudio.PyAudio()
-
+	#Opens stream
     stream = p.open(
         format = p.get_format_from_width(wf.getsampwidth()),
         channels = wf.getnchannels(),
         rate = wf.getframerate(),
         output = True)
     data = wf.readframes(chunk)
-
+	#Plays data/audio
     while data != '': 
         stream.write(data)
         data = wf.readframes(chunk)
-
+	#Closes stream after song finishes
     stream.close()
     p.terminate()
     
 def mainRun(fname,threshold):
+	#Main function for playing song and running light visualization 
+	
+	#Creates plan_sec from wav_analyzer
     plan_sec = wav_analyzer_fft(fname,threshold)
     
+	#Preps multiprocessing events for each function
     color_ready = multiprocessing.Event()
-    song_ready = multiprocessing.Event()
-    
+    song_ready = multiprocessing.Event()   
+	#Begins each separate event, runs both functions in sync
     songProcess = multiprocessing.Process(target=play_song,args = (fname, song_ready, color_ready))
-    songProcess.start()
-    
+    songProcess.start()  
     setColor_v2(plan_sec,color_ready,song_ready)
