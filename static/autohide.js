30adb824264422c8180a431580eb4ba9dbb6d26a
Jacob <bogdanovjacob@gmail.com>
2016-12-06 22:55:11 -0500

    added auto hiding header code

---
+++ static/autohide.js
@@ -0,0 +1,15 @@
+/*
+ * CS 411 Group 4
+ * alerts.js
+ * Shows and hides a bunch of divs in the HTML
+ * Inelegant code, hastily prepared by Karan Varindani
+ */
+
+// Hides everything but the two radio buttons to start
+
+$(document).ready(function() {
+	setTimeout(function() {
+        $('.message').fadeOut('fast');
+    }, 2000);
+	
+});
