0c64f7571a69b97416be8c76a1edb547e2b43cef
dgsienko <dgsienko@bu.edu>
2016-12-06 22:38:49 -0500

    added hex invert functionality

--- lights.py
+++ lights.py
@@ -71,5 +71,37 @@ def randomHex():
     	choice = random.choice(hexVal)
     	return choice
 
+def invertHex(hexNumber):
+    #invert a hex number
+    inverse = hex(abs(int(hexNumber, 16) - 255))[2:] 
+    # if the number is a single digit add a preceding zero
+    if len(inverse) == 1: 
+        inverse = '0'+inverse
+    return inverse
+    
+def hexInvert(hexCode):
+    #define an empty string for our new color code
+    inverse = "" 
+    # if the code is RGB
+    if len(hexCode) == 6: 
+        R = hexCode[:2]
+        G = hexCode[2:4]
+        B = hexCode[4:]
+    # if the code is ARGB
+    elif len(hexCode) == 8:
+        A = hexCode[:2]
+        R = hexCode[2:4]
+        G = hexCode[4:6]
+        B = hexCode[6:]
+        # don't invert the alpha channel
+        inverse = inverse + A 
+    else:
+        # do nothing if it is neither length
+        return hexCode 
+    inverse = inverse + invertHex(R)
+    inverse = inverse + invertHex(G)
+    inverse = inverse + invertHex(B)
+    return inverse
+
 
 
